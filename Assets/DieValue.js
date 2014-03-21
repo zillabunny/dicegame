5e6d044bac2020bfaf1d2848160c1f8855e18e5f
unknown <zillabunny@gmail.com>
2014-03-21 00:35:47 -0400

    first commit

---
+++ Assets/DieValue.js
@@ -0,0 +1,10 @@
+﻿public var currentValue = 0;
+
+function Update() {
+	
+	dieTextGameObject = GameObject.Find("DieText");
+	textMeshComponent = dieTextGameObject.GetComponent("TextMesh");
+	
+	textMeshComponent.text = currentValue.ToString();
+	
+}
---
+++ Assets/SideTrigger.js
@@ -0,0 +1,10 @@
+﻿public var faceValue=0;
+function OnTriggerEnter(other : Collider) 
+{
+	dieGameObject = GameObject.Find("SixSidedDie");
+	
+	dieValueComponent = dieGameObject.GetComponent("DieValue");
+	dieValueComponent.currentValue = faceValue;
+	Debug.Log(faceValue);
+}
+	
